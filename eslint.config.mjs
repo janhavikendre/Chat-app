4f46afe157f3bf42e5a1a10846ae32bff67e3539
KENDRE JANHAVI BHAGWAT <141988507+janhavikendre@users.noreply.github.com>
2025-01-12 13:37:23 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,8 @@ const eslintConfig = [
       "@next/next/no-typos": "off",
       "@next/next/no-unwanted-polyfillio": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars":"off"
+      "@typescript-eslint/no-unused-vars":"off",
+      "@typescript-eslint/no-unused-expressions":"off"
     }
   }
 ];
